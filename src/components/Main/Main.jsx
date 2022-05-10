e9497394652f5ec659b350bf485a49c6f6dc549b
stzaytsev <stzaytsev@ozon.ru>
2022-05-10 21:04:03 +0300

    fix main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,8 +5,8 @@ import MailPost from "../../UI/MailPost/MailPost";
 import '../../style/Main.css'
 import Modal from "../../UI/Modal/Modal";
 
-class Main extends React.Component{
-    componentDidMount = async () =>{
+class Main extends React.Component {
+    componentDidMount = async () => {
         const res = await fetch('http://localhost:3001//mailbox')
         const data = await res.json()
         this.setState({
@@ -21,17 +21,20 @@ class Main extends React.Component{
             }
         )
     }
+
     state = {
         modalMailVisible: false,
-        modalPost: {}
+        modalPost: {},
+        data: []
     }
+
     render() {
         return (
             <div>
                 <LeftMenu></LeftMenu>
                 <Navbar></Navbar>
-                {this.state.modalMailVisible ? <Modal onClose={() => this.setState({modalMailVisible: true})}
-                                                  post={this.state.data}/> : null}
+                {this.state.modalMailVisible ? <Modal onClose={() => this.setState({ modalMailVisible: true })}
+                    post={this.state.data} /> : null}
                 <div className="postsMail">
                     {this.state.data.map(post =>
                         <MailPost post={post} setPostData={this.setPostData}></MailPost>
